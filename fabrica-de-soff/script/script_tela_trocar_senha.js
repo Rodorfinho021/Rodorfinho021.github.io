29d4e944c44bf73118c298cd06b35086fa38d83e
Rodorfinho021 <160938761+Rodorfinho021@users.noreply.github.com>
2024-11-25 14:52:11 -0400

    Delete fabrica-de-soff directory

--- fabrica-de-soff/script/script_tela_trocar_senha.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('resetForm').addEventListener('submit', function (event) {
-    event.preventDefault(); // Prevent page reload
-    const email = document.getElementById('email').value;
-    if (email) {
-        alert("Código de confirmação enviado para: ",email);
-    } else {
-        alert('Por favor, insira um email válido.');
-    }
-});
